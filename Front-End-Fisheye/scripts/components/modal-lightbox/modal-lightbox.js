36540de37fa85abdbc043aa2ec3a6cb852c1946d
safina3d <safina3d@users.noreply.github.com>
2023-03-16 22:03:18 +0100

    loading message added

--- Front-End-Fisheye/scripts/components/modal-lightbox/modal-lightbox.js
+++ Front-End-Fisheye/scripts/components/modal-lightbox/modal-lightbox.js
@@ -29,6 +29,14 @@ class ModalLightboxComponent extends GenericComponent {
         return this.shadowRoot.getElementById('target-video');
     }
 
+    get title(){
+        return this.shadowRoot.querySelector("#photo-title");
+    }
+
+    get loader(){
+        return this.shadowRoot.querySelector("#loader");
+    }
+
     get previousButton(){
         return this.shadowRoot.querySelector("#previous-btn");
     }
@@ -37,9 +45,7 @@ class ModalLightboxComponent extends GenericComponent {
         return this.shadowRoot.querySelector("#next-btn");
     }
 
-    get title(){
-        return this.shadowRoot.querySelector("#photo-title");
-    }
+
 
     get currentCardImageElement(){
         return this._currentCardImageElement;
@@ -52,54 +58,83 @@ class ModalLightboxComponent extends GenericComponent {
         }
     }
 
-
     connectedCallback(){
         this.addEventListener('componentReadyEvent', this._doWhenReady);
     }
 
     disconnectedCallback(){
         this.removeEventListener('componentReadyEvent', this._doWhenReady);
-        this.removeEventListener('click', this._closeModal);
+        // this.removeEventListener('click', this._closeModal);
     }
 
     _doWhenReady(){
-        const closeBtn = this.shadowRoot.querySelector("#closeBtn");
-
-        // Handle close event
-        if (closeBtn) {
-            closeBtn.addEventListener('click', () => {
+        // Handle the close button click event
+        this.shadowRoot
+            .querySelector("#closeBtn")
+            .addEventListener('click', () => {
                 this._closeModal();
             });
-        }
 
+        // Handle the previous button click event
         this.previousButton.addEventListener("click", () => {
             this.currentCardImageElement = this.currentCardImageElement.previousElementSibling;
             this._updatePreview();
+            this._updateCtrollersVisibility();
         });
 
+        // Handle the next button click event
         this.nextButton.addEventListener("click", () => {
             this.currentCardImageElement = this.currentCardImageElement.nextElementSibling;
             this._updatePreview();
-        });
+            this._updateCtrollersVisibility();
 
+        });
     }
 
-    _updatePreview(){
-        if (this.currentCardImageElement){
-            const {target, title, isVideo } = this.currentCardImageElement.data;
-            this.imageTarget.src = isVideo ? "" : target;
-            this.videoTarget.src = isVideo ? target : "";
-            this.title.textContent = title;
-            this.imageTarget.hidden = isVideo;
-            this.videoTarget.hidden = !isVideo;
+    _updatePreview() {
+        if (this.currentCardImageElement) {
+            const { target, title, isVideo } = this.currentCardImageElement.data;
+            this.loader.style.display = "grid";
+
+            if (isVideo) {
+                this.videoTarget.src = target;
+                this.imageTarget.removeAttribute("src");
+            } else {
+                this.imageTarget.src = target;
+                this.videoTarget.removeAttribute("src");
+            }
+
+            const updateContentVisibility = () => {
+                this.imageTarget.hidden = isVideo;
+                this.videoTarget.hidden = !isVideo;
+                this.title.textContent = title;
+                this.loader.style.display = "none";
+            };
+
+            this.imageTarget.onload = updateContentVisibility;
+            this.videoTarget.onloadedmetadata = updateContentVisibility;
+            this._updateCtrollersVisibility();
         }
     }
+
     _closeModal(){
         this.isVisible = false;
         this.currentCardImageElement = null;
         this.imageTarget.src = "";
         this.videoTarget.src = "";
         this.title.textContent = "";
+    }
+
+    _updateCtrollersVisibility(){
+
+        this.currentCardImageElement.previousElementSibling
+            ? this.previousButton.classList.remove("hidden")
+            : this.previousButton.classList.add("hidden");
+
+        this.currentCardImageElement.nextElementSibling
+            ? this.nextButton.classList.remove("hidden")
+            : this.nextButton.classList.add("hidden");
+
     }
 
 }
